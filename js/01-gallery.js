db3731baac03e61fa3628030d09fa5cd17ef150e
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-03-29 09:23:59 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+for (const image of galleryItems) {
+  const listEl = `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src=${image.preview}
+        data-source=${image.original}
+        alt=${image.description}
+      />
+    </a>
+  </div>`;
+
+  galleryEl.insertAdjacentHTML('beforeend', listEl);
+  galleryEl.addEventListener('click', e => e.preventDefault());
+}
